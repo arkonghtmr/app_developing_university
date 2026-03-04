608e57627bd4a08b76c079cfb7c33dcca49ac43a
БЕЛЫЙ УИЛЛ СМИТ <antyatsina@yandex.ru>
2026-03-04 03:09:00 +0300

    pz3

---
+++ MireaProject/app/src/main/java/ru/mirea/fedorovas/mireaproject/WebViewFragment.java
@@ -0,0 +1,22 @@
+package ru.mirea.fedorovas.mireaproject;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class WebViewFragment extends Fragment {
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_web_view, container, false);
+        WebView webView = root.findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient()); // Чтобы открывалось внутри приложения
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.google.com");
+        return root;
+    }
+}
